49b094b4371327968ac348350688b7a8c30048f9
mpigelati <saimohansai@gmail.com>
2017-09-15 23:55:11 +0530

    adb devices,adb root is working

--- M_adb/myadb.py
+++ M_adb/myadb.py
@@ -22,8 +22,8 @@ class madb(object):
         #print(type(cmd))
         cmd_list.insert(0,self.__adbpath)
         cmd_list.insert(1,cmd)
-        #print(cmd_list)
-        print("generating build command and returning from __build_cmd_() function  "," ".join(cmd_list))
+        print(cmd_list)
+        #print("generating build command and returning from __build_cmd_() function  "," ".join(cmd_list))
         return " ".join(cmd_list)
 
     def run_cmd(self,cmd):
@@ -31,32 +31,53 @@ class madb(object):
         self._clear_()
         #self._build_cmd_(cmd)
         #print(self._build_cmd_(cmd))
-        self.__cmd=self._build_cmd_(cmd)
-        print("[cmd]",self.__cmd)
-        command = subprocess.check_output(self.__cmd,shell=True)
-        print ("type",type(command))
-        return  command
+        try:
+            self.__cmd = self._build_cmd_(cmd)
+            # print("[cmd]",self.__cmd)
+            command = subprocess.check_output(self.__cmd,shell=True)
+            #print ("type",type(command))
+            print ("command",command)
+            #print ("successfully executed build command",self.__cmd)
+            return  command
+        except OSError, e:
+           self.__error = str(e)
+            #self.__error = str(e)
+           print (self.__error)
+           print ("Failed to execute",self.__cmd)
 
     def device_filter(self,cmd):
-        print ("device_filter",cmd) #Output :- ('device_filter', 'List of devices attached \n7dab6ce3\tdevice\n\n')
+        #print ("device_filter",cmd) #Output :- ('device_filter', 'List of devices attached \n7dab6ce3\tdevice\n\n')
         dev = cmd.split(" ")[4]
-        dev = dev.split("\t")[0]
+        dev = dev.split("\t")[0].strip("\n"),
+        #dev=dev.rstrip("\n")
         #print dev
         self.__devices= dev
 
     def adb_devices(self):
         #print("adb devices")
-        self.__devices=None
-        cmd=self.run_cmd("devices")
-        #print cmd
-        self.device_filter(cmd)
-        print(self.__devices)
 
+        try:
+            self.__devices=None
+            cmd=self.run_cmd("devices")
+            #print cmd
+            self.device_filter(cmd)
+            print("cc",self.__devices)
+
+        except :
+            self.__devices-=None
+            print (self.__devices)
     def adb_root(self):
-        #print("adb devices")
-        self.__devices = None
-        self.run_cmd("root")
+        #print(a devices")
+        #self.__devices = None
+        try:
+            self.adb_devices()
+            if self.__devices !=None:
+               data = self.run_cmd("root")
+               print("root fun",data)
+        except OSError,e:
+            
 
+            self.__devices=None
 
 
 
--- sample/sample.py
+++ sample/sample.py
@@ -10,8 +10,10 @@ def main():
 
     #adb._data_()
     #adb.run_cmd()
-    adb.adb_devices()
-    #adb.adb_root()
+
+    #adb.adb_devices()
+    adb.adb_root()
+
 
 
 if __name__ == "__main__":
